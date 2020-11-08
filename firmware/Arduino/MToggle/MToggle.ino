b3c1a17e46322bf4d2c6da67cb0f86cfaa413e7a
p1nh0 <p1nh0.c0d1ng@gmail.com>
2020-11-08 19:45:27 +0000

    removed MController tester
    MController testers are redundant since the firmware allows to set a
    DEBUG variable which will print hardware state

--- firmware/Arduino/MToggle/MToggle.ino
+++ firmware/Arduino/MToggle/MToggle.ino
@@ -5,9 +5,11 @@
  *  Author: Tiago Ângelo (aka p1nh0)
 */
 
+#include <Wire.h>
+
 #define NUMBYTES 4 // number of bytes using in communication from M-Controllers to M-Brain
 #define BRAIN 8 // Slave Receiver address (M-Brain)
-#include <Wire.h>
+#define DEBUG 0
 
 char inputs[4] = {A0, A1, A2, A3}; // BUTTON PINS 
 byte leds[4] = {2, 3, 4, 7}; // LED PINS 
@@ -18,7 +20,8 @@ boolean changed = false; // used to check if toggles have changed
 
 void setup() {
   Wire.begin();// join i2c bus (address optional for master)
-   
+  if(DEBUG) Serial.begin(9600);
+  
   // hardware setup
   for (int i = 0; i < 4; i++){
     pinMode(inputs[i], INPUT_PULLUP); // button setup
@@ -41,7 +44,15 @@ void loop() {
   if(changed){
     Wire.beginTransmission(BRAIN);
     Wire.write(toggle, NUMBYTES);
-    Wire.endTransmission();  
+    Wire.endTransmission(); 
+
+    if(DEBUG){
+      for(int i=0; i < 4; i++){
+        Serial.print(toggle[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }     
   }
 
   changed = false; 
