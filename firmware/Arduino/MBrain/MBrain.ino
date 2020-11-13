5f4ecaf0af64b769beba515aa6c558b539219b43
p1nh0 <p1nh0.c0d1ng@gmail.com>
2020-11-13 23:00:25 +0000

    ported Atmel version to Arduino IDE

--- firmware/Arduino/MBrain/MBrain.ino
+++ firmware/Arduino/MBrain/MBrain.ino
@@ -1,87 +1,390 @@
 /* 
+ *  Version: 0.1.0 (protobrain)
+ *  Created: 12-11-2020
+ *  Author: Tiago Ângelo (aka p1nh0)
+*/
+
+/* 
  *  M-Brain (Slave Receiver) using Arduino Mega
- *  prototype version with four buttons (Note Map, CC Map, Load, Save
+ *  prototype version with four buttons (Note Map, CC Map, Load, Save)
  *  7 leds to display values from 0 to 127 in binary format
  *  rotary is connected to pins 2 and 3 
  *  rotary button is connected to pin 33 (with INPUT_PULLUP)
  *  buttons are connected to input pins 37, 36, 35 and 34 (with INPUT_PULLUP)
  *  buttons are connected to PORTC
  *  leds are connected to analog inputs A0 to A6 (PORTF)
- *  Created: 24-08-2019
- *  Author: Tiago Ângelo (aka p1nh0)
 */
 
+#pragma GCC push_options
+#pragma GCC optimize (OPTIMIZATION)
+
+// Libraries
 #include <Wire.h>
+#include <EEPROM.h>
 #include <MIDI.h>
 #include <Rotary.h>
+#include <TimerOne.h>
 
-#define ADDR 8 // this device address
-#define NUMBYTES 4 // number of bytes using in communication from M-Controllers to M-Brain
-#define PIN1 2 // rotary pins
-#define PIN2 3 // rotary pins
-#define MIDICH 1 // MIDI channel 
-#define DEBUG 1
+// My headers and classes 
+#include "GLOBALS.h"
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+void receiveI2C(int howmany);
+void resetMsg(); // reset I2C message
+void rotate();
+void timerISR();
+void michaelKnight(uint16_t t, boolean d);
+void blinkConfirm();
+//End of Auto generated function prototypes by Atmel Studio
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
 Rotary rotary = Rotary(PIN1, PIN2);
-// Counter that will beincremented or decremented by rotation.
-volatile byte counter = 0;
 
-byte toggle[4] = {0, 0, 0, 0}; // values received from M-Toggle
-byte innertoggle[4] = {0, 0, 0, 0}; // values stored in M-Brain to check for changes
-boolean received = false; 
-byte notemap[4] = {0, 1, 2, 3}; // map toggle values to MIDI notes  
-byte velmap[4] = {127, 127, 127, 127}; 
+volatile byte counter = 0; // Counter that will be incremented or decremented by rotation.
+volatile boolean received = false; 
+byte message[NUMBYTES]; // used to read I2C message
+byte maddr, mval; // easy access of I2C messages
+
+// event mapping arrays (stored and recalled from EEPROM) 
+uint8_t chmap[128];
+int8_t notemap[128];
+int8_t velmap[128];
+int8_t ccmap[128]; 
+uint8_t globalch = 1; // set global channel 
+uint8_t preset = 0; // current preset
+uint16_t loc = 0; // memory location
+boolean first = true; 
+
+boolean debug = false;
 
 void setup() {
   Wire.begin(ADDR); 
   Wire.onReceive(receiveI2C);
-  if(DEBUG) Serial.begin(9600);
-  MIDI.begin(MIDICH);
+  
   // Rotary interrupts
   attachInterrupt(digitalPinToInterrupt(PIN1), rotate, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN2), rotate, CHANGE);
-  Serial.print("interrupt pin1: ");
-  Serial.println(digitalPinToInterrupt(PIN1));
-  Serial.print("interrupt pin2: ");
-  Serial.println(digitalPinToInterrupt(PIN2));
+  if(debug) Serial.print("interrupt pin1: ");
+  if(debug) Serial.println(digitalPinToInterrupt(PIN1));
+  if(debug) Serial.print("interrupt pin2: ");
+  if(debug) Serial.println(digitalPinToInterrupt(PIN2));
   
-  DDRF = 0b11111111; // analog A0 to A7 as outputs
-  DDRC = 0b00000000; //
-  PORTC = 0xFF; // enable input pullup on pins 37 to 30
+  // Pin Setup
+  DDRF = 0b11111111; // analog A0 to A6 as outputs (leds)
+  DDRC = 0b11100000; // set pins 37 to 33 as inputs (buttons)
+  PORTC = 0b00011111; // enable input pullup on pins 37 to 33
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  // Debug
+  if(PINC == ENTER){
+  Serial.begin(BDRATE);
+  debug = true;
+  }
+  else if(debug) { Serial.begin(BDRATE); Serial.println("DEBUG MODE"); }
+  
+  // Midi  
+  MIDI.begin(globalch);
+  
+  // RESET PRESET MEMORY (see GLOBALS)
+  if(PINC == RESET){
+  for(int i = 0; i < 128; i++){
+    // reset channel map
+    chmap[i] = 0;
+    EEPROM.write(CHOFFSET + i, 0); 
+    // reset notemap
+    notemap[i] = -1;
+    EEPROM.write(NOTEOFFSET + i, -1); 
+    // reset velmap
+    velmap[i] = 127;
+    EEPROM.write(VELOFFSET + i, 127);
+    // reset ccmap
+    ccmap[i] = -1; 
+    EEPROM.write(CCOFFSET + i, -1); 
+  }
+  if(debug) Serial.println("RESET preset 0");
+  michaelKnight(100, 1);
+  }
+  else{
+    // LOAD machine previous state (aka Preset 0)
+    for(int i = 0; i < 128; i++){
+      chmap[i] = EEPROM.read(CHOFFSET + i);
+      notemap[i] = EEPROM.read(NOTEOFFSET + i);
+      velmap[i] = EEPROM.read(VELOFFSET + i);
+      ccmap[i] = EEPROM.read(CCOFFSET + i);
+    }
+    michaelKnight(100, 0);
+    if(debug) Serial.println("LOADED preset 0");
+  }
+  
 }
 
-void loop() {
-  PORTF = counter; // light leds
-  
-  if(received){ // if I2C data is received
-    
-    if(PINC != 0){ // if no buttons are pressed
-      for(int i = 0; i < 4; i++){
-        if(innertoggle[i] != toggle[i]){
-          innertoggle[i] = toggle[i];
-          if(toggle[i]>0)   MIDI.sendNoteOn(notemap[i], velmap[i], MIDICH);
-          else              MIDI.sendNoteOff(notemap[i], 0, MIDICH);
+void loop(){
+  if(received){
+    maddr = message[0]; // memory address (from 0 to 127, representing each controller of each M-Controller)
+    mval = message[1]; // controller value (ex: knob value, button, etc.)
+    if(debug){ Serial.print( "M-Controller ID:" ); Serial.println( get_maddr(maddr) ); Serial.print( "Controller#" );
+          Serial.print( get_ctrl(maddr) ); Serial.print( " = " ); Serial.println( mval );
+    } 
+  }
+
+  switch(PINC){
+    //############### NOTE MAP ###############################################
+    case NMAP:
+      if(debug) Serial.println("NOTE MAP MODE");
+      if(notemap[maddr] > -1) counter = notemap[maddr]; // set ENCODER value
+      else counter = 0; 
+      
+      if (!received)
+      {
+        Timer1.initialize(BLINK2X); 
+        Timer1.attachInterrupt( timerISR ); // blink LEDS 2x a second
+      }
+      
+      while(received){
+        Timer1.detachInterrupt();
+        PORTF = counter; // display value
+        
+        if(PINC == ENTER) { // STORE MAPPING
+          notemap[maddr] = counter; // store value in volatile memory (preset=0)
+          EEPROM.write(NOTEOFFSET + maddr, counter);
+          resetMsg();
+        }
+        else{ 
+          if(PINC == LOADB) { // CANCEL
+            resetMsg();
+          }
         }
       }
-    }
-    received = false; 
+      break;
+    
+    //############### VELOCITY MAP ###########################################
+    case VMAP:
+      if(debug) Serial.println("VELOCITY MAP MODE");
+      counter = velmap[maddr]; // set ENCODER value
+      
+      if (!received)
+      {
+        Timer1.initialize(BLINK4X);
+        Timer1.attachInterrupt( timerISR ); // blink LEDS 2x a second
+      }
+    
+      while(received){
+        Timer1.detachInterrupt();
+        PORTF = counter; // display value
+      
+        if(PINC == ENTER) {
+          velmap[maddr] = counter; // store value in volatile memory (preset=0)
+          EEPROM.write(VELOFFSET + maddr, counter);
+          resetMsg();
+        }
+        else{
+          if(PINC == LOADB) { // CANCEL
+            resetMsg();
+          }
+        }
+      }
+      break;
+    
+    //############### CC MAP #################################################
+    case CCMAP:
+      if(debug) Serial.println("CC MAP MODE");
+      
+      if(ccmap[maddr] > -1) counter = ccmap[maddr]; // set ENCODER value
+      else counter = 0; 
+      
+      if (!received)
+      {
+        Timer1.initialize(BLINK2X);
+        Timer1.attachInterrupt( timerISR ); // blink LEDS 2x a second
+      }
+    
+      while(received){
+        Timer1.detachInterrupt();
+        PORTF = counter; // display value
+      
+        if(PINC == ENTER) {
+          ccmap[maddr] = counter; // store value in volatile memory (preset=0)
+          EEPROM.write(CCOFFSET + maddr, counter);
+          resetMsg();
+        }
+        else{
+          if(PINC == LOADB) { // CANCEL
+            resetMsg();
+          }
+        }
+      }
+      break;
+    
+    //############### CH MAP #################################################
+    case CHMAP:
+      if(debug) Serial.println("CHANNEL MAP MODE");
+      counter = chmap[maddr]; // set ENCODER value
+      
+      if (!received)
+      {
+        Timer1.initialize(BLINK4X);
+        Timer1.attachInterrupt( timerISR ); // blink LEDS 2x a second
+      }
+    
+      while(received){
+        Timer1.detachInterrupt();
+        if(counter > 16) counter = 0;
+        PORTF = counter; // display value
+      
+        if(PINC == ENTER) {
+          chmap[maddr] = counter; // store value in volatile memory (preset=0)
+          EEPROM.write(CHOFFSET + maddr, counter);
+          resetMsg();
+        }
+        else{
+          if(PINC == LOADB) { // CANCEL
+            resetMsg();
+          }
+        }
+      }
+      break;
+      
+      //############### SET GLOBAL CHANNEL #################################
+      case GCHSET:
+        if(debug) Serial.println("SET GLOBAL CHANNEL");
+        counter = globalch; // reset ENCODER value
+        if (!received)
+        {
+          Timer1.initialize(BLINK2X);
+          Timer1.attachInterrupt( timerISR ); // blink LEDS 2x a second
+        }
+      
+        while(received){
+          Timer1.detachInterrupt();
+          if(counter > 16) counter = 1;
+          if(counter < 1) counter = 16;
+          PORTF = counter; // display value
+        
+          if(PINC == ENTER) {
+            globalch = counter; // store value in volatile memory (preset=0)
+            PORTF = 0x00; // turn all LEDS off
+            received = false;
+          }
+          else{
+            if(PINC == LOADB) { // CANCEL
+              PORTF = 0x00; // turn all LEDS off
+              received = false;
+            }
+          }
+        }
+        break;
+        //############### LOAD PRESET ####################################
+        case  LOADB:
+          // CANCEL (1st time press only!)
+          if(first){
+            Timer1.detachInterrupt(); // turn leds off if mapping mode is engaged but is the cancelled after pressing LOAD
+            PORTF = 0x00; // turn all LEDS off
+            preset = 0;
+            counter = 0;
+            first = false;  
+          } // CANCEL 
+          else{ // SELECT PRESET TO LOAD
+            if(counter > 7) counter = 0;
+            preset = counter;
+            PORTF = (BIT(preset) >> 1) ^ 0xFF; // unlit led represents selected preset, if all leds on preset=0 (0xFF = 0b11111111) 
+          }
+        break;
+        
+        case LOADYES:
+          for(int i = 0; i < 128; i++){
+            // load channel map
+            loc = (preset << 9) + CHOFFSET + i;
+            chmap[i] = EEPROM.read(loc);
+            // load note map
+            loc = (preset << 9) + NOTEOFFSET + i;
+            notemap[i] = EEPROM.read(loc);
+            // load velocity map
+            loc = (preset << 9) + VELOFFSET + i;
+            velmap[i] = EEPROM.read(loc);
+            // load cc map
+            loc = (preset << 9) + CCOFFSET + i;
+            ccmap[i] = EEPROM.read(loc);
+          }
+          blinkConfirm();
+        break;
+        
+        //############### SAVE PRESET ####################################
+        case SAVEB: // SELECT PRESET TO SAVE
+          if(counter > 7) counter = 0;
+          preset = counter;
+          PORTF = (BIT(preset) >> 1) ^ 0xFF; // unlit led represents selected preset, if all leds on preset=0 (0xFF = 0b11111111) 
+        break;
+        
+        case SAVEYES: // save preset 0 to a selected preset slot
+          for(int i = 0; i < 128; i++){  
+            // save channel map
+            loc = (preset << 9) + CHOFFSET + i;
+            EEPROM.write(loc, chmap[i]);
+            // save note map
+            loc = (preset << 9) + NOTEOFFSET + i;
+            EEPROM.write(loc, notemap[i]);
+            // save velocity map
+            loc = (preset << 9) + VELOFFSET + i;
+            EEPROM.write(loc, velmap[i]);
+            // save cc map
+            loc = (preset << 9) + CCOFFSET + i;
+            EEPROM.write(loc, ccmap[i]);
+          }
+          blinkConfirm();
+        break;
+        
+    //############### PLAY MODE ##############################################
+    default: // no buttons pressed
+      first = true; // reset (used for LOAD/CANCEL behaviour)
+      preset = 0; // reset preset to 0 if all buttons are released
+      PORTF = 0x00; // turn all LED's off
+      
+      if(received){ // if a message from a M-Controller is received
+        if(debug){ Serial.print("PLAY: "); Serial.print(maddr, BIN); Serial.print(" | "); Serial.println(mval); }
+    
+        uint8_t ch; // channel variable visible to PLAY MODE only
+        
+        if (chmap[maddr] > 0 ) ch = chmap[maddr]; // use chmap if there is one
+        else ch = globalch; // if channel is 0 then use global channel
+        
+        if (notemap[maddr] > -1 ) // if there's a notemap stored
+        {
+          if(mval > 0) MIDI.sendNoteOn(notemap[maddr], velmap[maddr], globalch);
+          else MIDI.sendNoteOff(notemap[maddr], 0, globalch);
+        }
+        
+        if (ccmap[maddr] > -1 ) // if there's a ccmap stored
+        {
+          MIDI.sendControlChange(ccmap[maddr], mval, globalch);
+        }
+        
+        received = false; 
+      }
+    break;
   }
-  
 }
 
+// called whenever an I2C message is received
 void receiveI2C(int howmany){
-  byte index = 0; 
-  received = true; 
+  byte index = 0;
   while(Wire.available()){
     byte v = Wire.read();
-    toggle[index] = v;
+    message[index] = v;
     index++;
-    if(DEBUG) Serial.print(v);
-    if(DEBUG) Serial.print(" "); 
   }
-  if(!Wire.available()) Serial.println(); // print line when no more bytes are available
+  received = true;
+}
+
+// reset I2C message
+void resetMsg(){
+  PORTF = 0x00; // turn all LEDS off
+  received = false;
+  maddr = -1;
+  mval = -1;
+  message[0] = -1;
+  message[1] = -1; 
 }
 
 // rotate is called anytime the rotary inputs change state.
@@ -89,9 +392,69 @@ void rotate() {
   volatile unsigned char result = rotary.process();
   if (result == DIR_CW) {
     counter++;
-    if(DEBUG) Serial.println(counter);
+  // fold from 0 to 127
+  if (counter > 127) counter = 0;
   } else if (result == DIR_CCW) {
     counter--;
-    if(DEBUG) Serial.println(counter);
+  // fold from 0 to 127
+  if (counter > 127) counter = 127;
   }
+}
+
+// timerISR uses TimerOne
+void timerISR(){
+  // toggle all 7-LED's 
+  PORTF ^= 0b01111111; 
+}
+
+// Michael Knight LED movement
+void michaelKnight(uint16_t t, boolean d){
+  if(d<1){ // right-to-left direction
+    for(int i=0; i < 7; i++){
+      PORTF = 1 << i;
+      delay(t);
+    }
+    for(int i=1; i < 8; i++){
+      PORTF = 64 >> i;
+      delay(t);
+    }
+  }
+  else{ //left-to-right direction
+    for(int i=0; i < 7; i++){
+      PORTF = 64 >> i;
+      delay(t);
+    }
+    for(int i=1; i < 8; i++){
+      PORTF = 1 << i;
+      delay(t);
+    }
+  }
+  PORTF = 0x00; // safely set port to 0 (leds off)
+}
+
+void blinkConfirm(){
+  PORTF = 0x00;
+  
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  PORTF ^= 0b01111111;
+  delay(100);
+  
+  PORTF = 0x00;
 }
