bec3764da8738a586b23a2af7d0cf8facfb4e345
p1nh0 <p1nh0.c0d1ng@gmail.com>
2021-06-15 00:44:21 +0100

    starting M-Encoder-C atmel studio project

---
+++ firmware/Atmel/M-Encoder-C/M-Encoder-C/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 15-06-2021 00:38:48
+ * Author : tiago
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
